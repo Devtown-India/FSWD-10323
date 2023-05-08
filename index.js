3c18c6428816e6f17b4694501900edf4c93381d2
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-08 18:29:54 +0530

    fs

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const mathmodule = require('./math')
-console.log(mathmodule)
+const fs = require('fs');
+
+const text = 'this is text'
+
+const data = fs.readFileSync('temp.txt')
+console.log(data.toString())
