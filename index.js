f104f4e6499bc0ea59b94bb5f4b4a242d3e984e6
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-09 18:28:06 +0530

    dependencies

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const math = require('./math')
+const { v4: uuidv4 } = require('uuid');
+const fetch = require('node-fetch')
 
-console.log(math.add(1, 2,22,45,23,45,67,89,90,100))
+const id = uuidv4(); 
+console.log(id)
+
+const fetchData = async()=>{
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+fetchData()
+
+// console.log(fetch)
--- readme.md
+++
@@ -1,4 +0,0 @@
-## Course Material Course Playlist : https://www.youtube.com/playlist?list=PL7zl8TDRnbunSi4YEWWlRHCeoxXtiIFr- 
-## Github : https://github.com/ShapeAI/FSWD-10323
-## My socials 
-Github : https://github.com/Kartik18g LinkedIn : https://www.linkedin.com/in/kartik18g/
