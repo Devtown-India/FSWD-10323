c82d82b30944f425a0e9133859c93df64d56bd90
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-11 19:08:58 +0530

    req res in express

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 
-const PORT = 8080
+const PORT = 8081
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    console.log(req.headers)
+    console.log(req.path)
+
+    console.log('Hello World')
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
