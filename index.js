42e5b0de40ed9f6bf188cc985a2de35330cba47e
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-12 18:54:45 +0530

    params and query

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-const express = require('express')
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-const PORT = 8081
+const PORT = 8081;
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send('First response from express  !!')
+app.get("/", (req, res) => {
+  res.status(200).send("First response from express  !!");
+});
+
+app.get('/product/:name', (req, res) => {
+    console.log(req.params)
+    res.status(200).send(`My name is ${username}`);
 })
 
-app.get('/todos',(req,res)=>{
-    fs.readFile('./db.js','utf-8',(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        res.status(200).json(
-            JSON.parse(data)
-        )
-        
-    })
-})
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.get("/todos", (req, res) => {
+  const { count } = req.query;
+  if (count) {
+    fs.readFile("./db.js", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(200).json(JSON.parse(data).slice(0, count));
+    });
+  } else {
+    fs.readFile("./db.js", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(200).json(JSON.parse(data));
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
