5e07ef76acaac8d9922d44417d54feb0575161d3
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-22 19:08:39 +0530

    delete api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const uuid = require("uuid");
 
 const PORT = 8081;
 
@@ -43,19 +44,59 @@ app.get("/todos", async (req, res) => {
 
 // create an endpoint to add a todo
 app.post("/todos", async (req, res) => {
-  console.log(req.body);
-  const { title } = req.body;
-  const todo = {
-    title: title,
-    isComplete: false,
-  };
-  const data = await fs.promises.readFile("./db.json", "utf-8");
-  const parsedData = JSON.parse(data);
-  parsedData.push(todo);
-  await fs.promises.writeFile("./db.json", JSON.stringify(parsedData));
-  return res.json(req.body);
-  // const {isComplete,title} = req.body;
+  try {
+    console.log(req.body);
+    const { title } = req.body;
+    const todo = {
+      title: title,
+      isComplete: false,
+      id: uuid.v4(),
+    };
+    const data = await fs.promises.readFile("./db.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    parsedData.push(todo);
+    await fs.promises.writeFile("./db.json", JSON.stringify(parsedData));
+    return res.status(200).json({
+      message: "Successfully fetched the todos",
+      data: parsedData,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      data: null,
+    });
+  }
 });
+
+// create an endpoint to delete a todo
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await fs.promises.readFile("./db.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    const todo = parsedData.find((todo) => todo.id === id);
+    if(todo){
+      // delete the todo from array
+      const filteredData = parsedData.filter((todo) => todo.id !== id);
+      await fs.promises.writeFile("./db.json", JSON.stringify(filteredData));
+      return res.status(200).json({
+        message:"Todod deleted successfully",
+        data: filteredData
+      })
+    }
+     return res.status(500).json({
+      message: "Todo with this id does not exist",
+      data: null,
+    });
+  } catch (error) {
+     return res.status(500).json({
+      message: "Internal Server Error",
+      data: null,
+    });
+  }
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
