437d04c59c1533c8346c0773ecb97d333f565f84
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-22 18:43:24 +0530

    req body

--- index.js
+++ index.js
@@ -5,17 +5,56 @@ const PORT = 8081;
 
 const app = express();
 
-const logger = (req,res,next)=>{
-    console.log(`Request method: ${req.method} and request path: ${req.path}`)
-    next()
-}
+const logger = (req, res, next) => {
+  console.log(`Request method: ${req.method} and request path: ${req.path}`);
+  next();
+};
 
-app.use(logger)
+app.use(logger);
 
 app.get("/ping", (req, res) => {
   res.status(200).send("PONG");
 });
 
+// create an endpoint to get all of the todos
+
+/**
+ response body structure
+ * status: 200
+ * data: {}
+ * message: "Success"
+ */
+
+app.get("/todos", async (req, res) => {
+  try {
+    fs.readFile("./db.json", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: "Internal Server Error",
+          data: null,
+        });
+      }
+      return res.status(200).json({
+        message: "Successfully fetched the todos",
+        data: JSON.parse(data),
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      data: null,
+    });
+  }
+});
+
+// create an endpoint to add a todo
+app.post('/todos',async (req,res)=>{
+  console.log(req.body)
+  return res.json(req.body)
+  // const {isComplete,title} = req.body;
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
