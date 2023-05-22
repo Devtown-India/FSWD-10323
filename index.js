d6da106eecfb1bc3051d187b389dbc525791f5a5
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-22 18:29:58 +0530

    boilerplate for day 38

--- index.js
+++ index.js
@@ -5,47 +5,17 @@ const PORT = 8081;
 
 const app = express();
 
-const logUserAgent = (req,res,next)=>{
-    const agent = req.headers['user-agent']
-    if(agent.includes('Chrome'))return res.status(403).send("Unauthorised")
-    next()
-}
-
 const logger = (req,res,next)=>{
     console.log(`Request method: ${req.method} and request path: ${req.path}`)
     next()
 }
 
-app.use(logUserAgent)
 app.use(logger)
 
-app.get("/", (req, res) => {
-  res.status(200).send("First response from express  !!");
+app.get("/ping", (req, res) => {
+  res.status(200).send("PONG");
 });
 
-app.get('/product/:name', (req, res) => {
-    const {name} = req.params
-    res.status(200).send(`My name is ${name}`);
-})
-
-app.get("/todos", (req, res) => {
-  const { count } = req.query;
-  if (count) {
-    fs.readFile("./db.js", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      res.status(200).json(JSON.parse(data).slice(0, count));
-    });
-  } else {
-    fs.readFile("./db.js", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      res.status(200).json(JSON.parse(data));
-    });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
