9bb4e3e2c2a427ff70e55220543be47312722de3
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-26 18:15:21 +0530

    boilerplate

--- server/index.js
+++ server/index.js
@@ -3,32 +3,12 @@ const fs = require("fs");
 const uuid = require("uuid");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
-const initializedb = require("./utils/initializedb");
-const ls = require("local-storage");
 
 const SECRET_KEY = "secret_key_sdf";
-
-/*
-  User schema
-    id
-    name
-    email
-    password
- */
-
-/*
-  Todo schema
-    id
-    user
-    title
-    isComplete
- */
 
 const PORT = 8081;
 
 const app = express();
-initializedb();
 
 const logger = (req, res, next) => {
   console.log(`Request method: ${req.method} and request path: ${req.path}`);
@@ -37,93 +17,30 @@ const logger = (req, res, next) => {
 app.use(cors());
 app.use(logger);
 app.use(express.json());
-app.use(cookieParser());
 
-const authorization = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return res.redirect('/login');
-  }
-  try {
-    const data = jwt.verify(token, SECRET_KEY);
-    req.userId = data.id;
-    req.userEmail = data.email;
-    return next();
-  } catch {
-    // token has expired
-    return res.redirect('/logout');
-  }
-};
-
-app.get("/", authorization, (req, res) => {
-  console.log({
-    email: req.userEmail,
-    id: req.userId,
-  });
-  res.send(`Hi ${req.userEmail} !!`);
+app.get("/",(req, res) => {
+  res.send(`Hi user!!`);
 });
 
 app.get("/login", (req, res) => {
-  const token = req.cookies.access_token;
-  if (token) {
-    return res.redirect("/");
-  }
   res.sendFile(__dirname + "/pages/login.html");
 });
 
 app.get("/signup", (req, res) => {
-  const token = req.cookies.access_token;
-  if (token) {
-    return res.redirect("/");
-  }
   res.sendFile(__dirname + "/pages/signup.html");
 });
 
 app.get('/logout', (req, res) => {
-  res.clearCookie('access_token')
-  res.redirect('/login')
+   res.send('route to logout')
+
 })
 
-
-app.post("/api/signup", (req, res) => {
-  const { email, password, name } = req.body;
-  const users = ls.get("users");
-  if (users.find((user) => user.email === email)) {
-    return res.status(400).send("User already exists");
-  }
-  const newUser = {
-    id: uuid.v4(),
-    name,
-    email,
-    password,
-  };
-  users.push(newUser);
-
-  ls.set("users", users);
-  return res.status(200).send("User created");
+app.post("/api/login", (req, res) => {
+  res.send('api to login !!')
 });
 
-app.post("/api/login", (req, res) => {
-  const { email, password } = req.body;
-  const users = ls.get("users");
-  const user = users.find((user) => user.email === email);
-  if (!user) {
-    return res.status(400).send("User not found");
-  }
-  if (user.password !== password) {
-    return res.status(400).send("Password is incorrect");
-  }
-
-  const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, {
-    expiresIn: "1h",
-  });
-
-  return res
-    .cookie("access_token", token, {
-      httpOnly: true,
-    })
-    .status(200)
-    .json({ message: "Logged in successfully" });
+app.post("/api/signup", (req, res) => {
+  res.send('api to signup !!')
 });
 
 app.listen(PORT, () => {
