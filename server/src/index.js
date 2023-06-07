71c3471a6af9c17d7076431b3f999a1aa4592144
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-07 18:43:09 +0530

    log config

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,23 @@
-const {add} = require('./utils/add')
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import logger, { morganMiddleware } from './logger'
+import cookieParser from 'cookie-parser'
 
-console.log(add(1, 2, 3, 4, 5,99))
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.use(morganMiddleware)
+
+app.get('/', (req, res) =>{
+    res.send(`Server is running `)
+})
+
+app.listen(PORT, () => {
+    logger.info(`Server is listening on port ${PORT}`)
+})
---
+++ server/src/logger/index.js
@@ -0,0 +1,64 @@
+import morgan from "morgan";
+import winston from "winston";
+import path from "path";
+
+const { format } = winston;
+const { combine, timestamp, printf, colorize, align, json } = format;
+
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'warn';
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+const logFormat =  winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const logger = winston.createLogger({
+  format: logFormat,
+  level: level(),
+  levels,
+  transports: [
+    new winston.transports.Console({ level: "http" }),
+    new winston.transports.File({
+      filename: path.resolve(__dirname, "../logs/app-error.log"),
+      level: "error",
+      format: combine(timestamp(), json()),
+    }),
+  ],
+});
+
+export const morganMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  {
+    stream: {
+      write: (message) => logger.http(message.trim()),
+    },
+  }
+);
+
+export default logger;
