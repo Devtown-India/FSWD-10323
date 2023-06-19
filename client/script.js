e314ea24302c8ddbc9621c483087e76c1246371e
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-19 19:12:53 +0530

    react js

--- client/script.js
+++ client/script.js
@@ -1,18 +1,36 @@
 console.log(React);
 console.log(ReactDOM);
 
-const reactElement = React.createElement(
-  "div",
+const Navbar = React.createElement(
+  "nav",
   {},
-  React.createElement('h1', {}, "Hello World!"),
   React.createElement(
     "ul",
-    {},
-    React.createElement("li", {}, "One"),
-    React.createElement("li", {}, "Two"),
-    React.createElement("li", {}, "Three")
+    { className: "nav-links" },
+    React.createElement(
+      "li",
+      {
+        onClick: () => console.log("clicked"),
+        className: "nav-link",
+      },
+      "Home"
+    ),
+    React.createElement("li", {}, "About"),
+    React.createElement("li", {}, "Contact")
   )
 );
 
+const Content = ({ data }) => React.createElement("div", {}, data);
+
+const Footer = React.createElement("div", {}, "footer");
+
+const App = React.createElement(
+  "div",
+  {},
+  Navbar,
+  React.createElement(Content, { data: "lorem ipsum" }),
+  Footer
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactElement);
+root.render(App);
