8fc6df931e2982d16cb6a5c9ec71156b264702ff
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-22 17:48:19 +0530

    todo list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,28 @@ import Header from "./components/Header";
 import List from "./components/List";
 import Footer from "./components/Footer";
 
-
 const App = () => {
 
-  /*
-    * title
-    * complete
-    * id
-  */
-
-  const [todos,setTodos] = useState([])
-  console.log(todos)
+  const [todos, setTodos] = useState([])
 
   return (
-    <div>
-      <Header setTodos={setTodos} />
-      <List setTodos={setTodos} todos={todos} />
+    <div className="antialiased  bg-slate-200 text-slate-700 mx-2">
+      <div class="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+        <div class="flex flex-col justify-between items-center">
+          <div>
+            <h1 class="text-3xl font-medium">Todo list</h1>
+          </div>
+          <Header />
+        </div>
+
+        <p class="text-slate-500">{todos.length!==0?`Hello, here are your latest todos`:`Add todos to get started`} </p>
+        <div id="todo-container..."></div>
+
+        <p class="text-xs text-slate-500 mt-4 text-center">
+          Last updated
+          <span id="last-updated">{new Date().toDateString()}</span>
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,11 +20,22 @@ const Header = ({ setTodos }) => {
   };
 
   return (
-    <header>
-      <h1>Todo list</h1>
-      <input onChange={handleChange} type="text" />
-      <button onClick={handleSubmit}>Add</button>
-    </header>
+      <div class="relative w-full my-3">
+        <input
+          type="search"
+          id="add-todo"
+          class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Add item to your todo list"
+          required
+        />
+        <button
+          type="submit"
+          id="add-todo-btn"
+          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Add
+        </button>
+      </div>
   );
 };
 export default Header;
