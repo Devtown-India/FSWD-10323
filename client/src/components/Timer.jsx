0d994e1bd2d19aadc95dd0d696d546deb2446863
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-23 18:31:37 +0530

    lifecycle

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Timer = () => {
-    return ( 
-        <div>
-            <h1>Timer</h1>
-        </div>
-     );
-}
- 
+  const [time, setTime] = useState(0);
+
+  const interval = setInterval(() => {
+    setTime((prev) => prev + 1);
+  }, 1000);
+
+  return (
+    <div>
+      <h1>Timer</h1>
+      {time}
+      {/* <h2 className="text-lg text-red-300 bold">{count}</h2>
+      <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+        Inc
+      </button> */}
+    </div>
+  );
+};
+
 export default Timer;
--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -1,11 +1,26 @@
-const Navabar = ({changeView}) => {
-    return ( 
-        <ul className="flex justify-center" > 
-            <li onClick={()=>changeView('home')} className="text-indigo-500 underline mx-4" >Home</li>
-            <li onClick={()=>changeView('posts')} className="text-indigo-500 underline mx-4">Posts</li>
-            <li  onClick={()=>changeView('timer')}className="text-indigo-500 underline mx-4">Timer</li>
-        </ul>
-     );
-}
- 
+const Navabar = ({ changeView }) => {
+  return (
+    <ul className="flex justify-center">
+      <li
+        onClick={() => changeView("home")}
+        className="text-indigo-500 hover:cursor-pointer underline mx-4"
+      >
+        Home
+      </li>
+      <li
+        onClick={() => changeView("posts")}
+        className="text-indigo-500 hover:cursor-pointer underline mx-4"
+      >
+        Posts
+      </li>
+      <li
+        onClick={() => changeView("timer")}
+        className="text-indigo-500 hover:cursor-pointer underline mx-4"
+      >
+        Timer
+      </li>
+    </ul>
+  );
+};
+
 export default Navabar;
