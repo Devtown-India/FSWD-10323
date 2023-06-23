1f469e7c15c2419d238d791a72890c8f035181cc
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-23 18:44:36 +0530

    side effects

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Timer = () => {
-  const [time, setTime] = useState(0);
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("John");
 
-  const interval = setInterval(() => {
-    setTime((prev) => prev + 1);
-  }, 1000);
+  const initialiseTimer = () => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    // this only executes on mount
+    initialiseTimer();
+  }, []);
 
   return (
     <div>
       <h1>Timer</h1>
-      {time}
+      {count}
       {/* <h2 className="text-lg text-red-300 bold">{count}</h2>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+
       <button type="button" onClick={() => setCount((prev) => prev + 1)}>
         Inc
-      </button> */}
+      </button>
+      <div>{name}</div> */}
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
