0e3280457c253767ee0a66f7e7cc73da05126d40
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-28 19:03:31 +0530

    Link in react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,18 @@ import { Route, Routes } from "react-router";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import { Link } from "react-router-dom";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 const App = () => {
   return (
     <div>
-      <ul className="flex my-4">
-        <li>
-          <Link className="underline" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="underline" to="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Navbar = ({ changeView }) => {
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
@@ -37,22 +38,21 @@ const Navbar = ({ changeView }) => {
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="#"
+              <Link
+                to={"/"}
                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
-
             <li>
-              <a
-                href="#"
+              <Link
+                to={"/contact"}
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
