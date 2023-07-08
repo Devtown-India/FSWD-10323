4516324b4faf18097ca103776eb14be39030da63
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-08 18:17:03 +0530

    delete todo

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,14 +1,24 @@
 import { useContext } from "react";
 import { TodoContext } from "../contexts/Todo";
+import { useSelector, useDispatch } from "react-redux";
 
 const List = () => {
-  const context = useContext(TodoContext);
-  const { todos } = context;
+  // const context = useContext(TodoContext);
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch({
+      type: "DELETE_TODO",
+      payload: id,
+    });
+  };
   return (
     <div>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <li onClick={() => handleDelete(todo.id)} key={todo.id}>
+            {todo.title}
+          </li>
         ))}
       </ul>
     </div>
--- client/src/reducers/todo.js
+++ client/src/reducers/todo.js
@@ -20,6 +20,11 @@ export const todoReducer = (state = initialState, action) => {
                     complete:false
                 }]
             }
+        case 'DELETE_TODO':
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.payload)
+            }
         default:
             return state
     }
