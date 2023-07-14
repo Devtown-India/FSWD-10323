ecd6d84f4bcade73c58cc60c3eda57b33974de32
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-14 18:54:34 +0530

    boilerplate for major proj with axio

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -1,32 +1,32 @@
 const Footer = () => {
   return (
     <div>
-      <footer class="left-0 z-20 w-full p-4 bg-gray-900 border-t border-gray-900 shadow md:flex md:items-center md:justify-between md:p-6">
-        <span class="text-sm text-white sm:text-center">
+      <footer className="left-0 z-20 w-full p-4 bg-gray-900 border-t border-gray-900 shadow md:flex md:items-center md:justify-between md:p-6">
+        <span className="text-sm text-white sm:text-center">
           © 2023{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
-            Flowbite™
+          <a href="https://flowbite.com/" className="hover:underline">
+            Donut™
           </a>
           . All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
+            <a href="#" className="mr-4 hover:underline md:mr-6">
               About
             </a>
           </li>
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
+            <a href="#" className="mr-4 hover:underline md:mr-6">
               Privacy Policy
             </a>
           </li>
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
+            <a href="#" className="mr-4 hover:underline md:mr-6">
               Licensing
             </a>
           </li>
           <li>
-            <a href="#" class="hover:underline">
+            <a href="#" className="hover:underline">
               Contact
             </a>
           </li>
---
+++ client/src/utils/axios/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    timeout: 5000,
+    headers: {},
+});
+
+instance.interceptors.request.use({
+    success: (config) => {
+        return config;
+    },
+    error: (error) => {
+        // Do something with request error
+        return Promise.reject(error);
+    },
+});
+
+instance.interceptors.response.use({
+    success: (response) => {
+        return response.data;
+    },
+    error: (error) => {
+        // Do something with request error
+        return Promise.reject(error);
+    },
+});
+
+export default instance;
