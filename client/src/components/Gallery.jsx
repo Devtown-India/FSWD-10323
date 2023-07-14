706d70549c40837c8ecafa2e027e2fb14dbcb27b
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-14 19:32:06 +0530

    intersection observer

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,5 +1,81 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
 const Gallery = () => {
-  return <div>home</div>;
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const lastImageRef = useRef(null);
+  const observer = useRef(null);
+  const limit = 20;
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`
+      );
+      setPosts((prev) => [...prev, ...data]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  useEffect(() => {
+    if (loading) return;
+    if (observer.current) {
+      // if observer is already observing then disconnect it
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      {
+        threshold: 0,
+      }
+    );
+    if (lastImageRef.current) {
+      observer.current.observe(lastImageRef.current);
+    }
+  }, [posts, loading]);
+
+  const images = [
+    { src: "https://picsum.photos/200/300" },
+    { src: "https://picsum.photos/100/300" },
+    { src: "https://picsum.photos/300/300" },
+    { src: "https://picsum.photos/200/100" },
+    { src: "https://picsum.photos/100/300" },
+
+    { src: "https://picsum.photos/200/300" },
+  ];
+  return (
+    <>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4, 1100: 5 }}
+      >
+        <Masonry>
+          {posts.map(({ url, id }, index) => (
+            <img
+              ref={index === posts.length - 1 ? lastImageRef : null}
+              className="m-2"
+              key={id}
+              src={url}
+              alt="random"
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+      {loading && <h1>Loading...</h1>}
+    </>
+  );
 };
 
 export default Gallery;
