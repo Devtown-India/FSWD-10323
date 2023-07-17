16713e1fa2d6bb3a900f0e1c3cb73e5615c3191b
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-17 18:47:28 +0530

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./redux/stores";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
   </BrowserRouter>
 );
---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,14 @@
+const initialState = {
+  token:null,
+  user:null,
+  loaded:false,
+}
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default authReducer
