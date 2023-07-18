4840871276aa507bdfb25a48017cb31604352671
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-18 18:28:47 +0530

    load user on refresh or first load

--- client/src/redux/actions/auth/index.js
+++ client/src/redux/actions/auth/index.js
@@ -5,14 +5,14 @@ export const loginUser = (user) => {
   return async (dispatch) => {
     try {
       const response = await axios.post("/auth/login", user);
-      const { data: responseData } = response;
-      const { token } = responseData.data;
+      const responseData  = response.data;
+      const { token } = responseData;
       axios.defaults.headers.common["Authorization"] = `${token}`;
       // store token in local storage
       localStorage.setItem("token", token);
       dispatch({
         type: "LOGIN_USER",
-        payload: responseData.data,
+        payload: responseData,
       });
       toast.success("Login successful");
     } catch (error) {
@@ -28,13 +28,12 @@ export const loadUser = () => {
       const token = localStorage.getItem("token");
       if (!token) return;
       const response = await axios.get(`/auth/validate-token/${token}`);
-      const { data: responseData } = response;
-      console.log(responseData);
+      const responseData  = response.data;
       dispatch({
         type: "LOAD_USER",
         payload: {
           token,
-          user: responseData.data,
+          user: responseData,
         },
       });
     } catch (error) {
--- client/src/utils/axios/index.js
+++ client/src/utils/axios/index.js
@@ -1,30 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
-    timeout: 5000,
-    headers: {},
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  timeout: 5000,
+  headers: {},
 });
 
-instance.interceptors.request.use({
-    success: (config) => {
-        return config;
-    },
-    error: (error) => {
-        // Do something with request error
-        return Promise.reject(error);
-    },
-});
-
-instance.interceptors.response.use({
-    success: (response) => {
-        console.log(response)
-        return response.data;
-    },
-    error: (error) => {
-        // Do something with request error
-        return Promise.reject(error);
-    },
-});
+instance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
